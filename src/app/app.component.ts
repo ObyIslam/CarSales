7dfd1c6c5607ba4f660eece421d4e633b6ad1c8b
ObyIslam <s00235259@atu.ie>
2024-04-10 23:21:24 +0100

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
+import { CarlistComponent } from './components/carlist/carlist.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CarlistComponent,CarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
